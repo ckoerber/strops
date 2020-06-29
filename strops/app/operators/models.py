339624e6f2658a3ae451f4b1e95e128f6e068b78
ckoerber <software@ckoerber.com>
2020-06-29 08:58:46 +0200

    Updated model relationships and added documentation

--- strops/app/operators/models.py
+++ strops/app/operators/models.py
@@ -1,16 +1,13 @@
-"""Models of operators
-"""
-
-# Note: if you want your models to use espressodb features, they must inherit from Base
-
+"""Table implementations related to operators."""
 from django.db import models
 from espressodb.base.models import Base
 
+from sympy import S, Symbol
 
-class DOF(Base):
-    """Degrees of freedom present in operators."""
 
-    # verbose_name = "Degree of freedom"
+class Field(Base):
+    """Field representing a degree of freedom present in operators."""
+
     kind = models.CharField(
         max_length=256, help_text="Kind of operator like 'quark'  or 'nucleon'."
     )
@@ -22,6 +19,13 @@ class DOF(Base):
         help_text="Which version of d.o.f., e.g.,"
         " specifying flavor ('up') or strong isospin ('proton').",
     )
+    symbol = models.CharField(
+        max_length=24, help_text="Mathematical symbol representing the d.o.f.",
+    )
+
+    def check_consistency(self):
+        """Checking if d.o.f. can be converted to a symbol."""
+        Symbol(self.symbol)
 
     class Meta:
         """Constraints on class."""
@@ -35,9 +39,14 @@ class Operator(Base):
     Includes spin and chirality, does not include factors and isospin/flavor
     """
 
-    name = models.CharField(max_length=256)
-    dof = models.ManyToManyField(DOF)
-    n_dofs = models.PositiveIntegerField()
+    name = models.CharField(
+        max_length=256, help_text="Name of the operator which can be used for searches."
+    )
+    fields = models.ManyToManyField(
+        Field,
+        through="Basis",
+        help_text="Fields present in the operator (e.g., up-quark fields).",
+    )
     expression = models.TextField(
         unique=True,
         help_text="Fully qualified term discribing operator"
@@ -56,37 +65,114 @@ class Operator(Base):
         help_text="Which kind of spin interactions describe this operator?",
     )
     # discrete symmetries
-    parity = models.IntegerField()
-    time = models.IntegerField()
-    charge = models.IntegerField()
+    charge = models.IntegerField(
+        help_text="Charge transformation behavior of operator."
+    )
+    parity = models.IntegerField(
+        help_text="Parity transformation behavior of operator."
+    )
+    time = models.IntegerField(help_text="Time transformation behavior of operator.")
     # Further details
-    # details = models.JSONField()
+    details = models.JSONField(
+        null=True,
+        blank=True,
+        help_text="Further optional information to specify the operator.",
+    )
 
     def __str__(self):
         """Returns expression."""
         return self.expression
 
+    def check_consistency(self):
+        """Checks for operator before saved:
 
-class Reference(Base):
-    """
-    """
+        * expression can be converted to sympy expression
+        * expression contains expected numbers of d.o.fs
+        """
+        S(self.expression)
 
-    arxiv_id = models.CharField(max_length=20)
-    authors = models.TextField()
-    journal = models.CharField(max_length=256)
-    title = models.CharField(max_length=256)
-    # misc = models.JSONField()
+
+class Basis(Base):
+    """Basis table associating field entries with operator entries."""
+
+    field = models.ForeignKey(
+        Field,
+        on_delete=models.CASCADE,
+        help_text="Field present in operator representation.",
+    )
+    operator = models.ForeignKey(
+        Operator, on_delete=models.CASCADE, help_text="The operator to represent."
+    )
+    n_fields = models.PositiveIntegerField(
+        default=1, help_text="Number of field occurances."
+    )
+
+    class Meta:
+        """Constraints on class."""
+
+        unique_together = ["field", "operator", "n_fields"]
+
+
+class Publication(Base):
+    """Publication as a reference for specifying information about operators."""
+
+    arxiv_id = models.CharField(
+        max_length=20, help_text="Arxiv qualifier like '2000.01234'."
+    )
+    authors = models.TextField(
+        help_text="Authors of the reference as comma seperated list."
+    )
+    title = models.CharField(max_length=256, help_text="Title of the publication.")
+    journal = models.CharField(
+        null=True,
+        blank=True,
+        max_length=256,
+        help_text="Journal qualifier of the publication.",
+    )
+    url = models.URLField(
+        null=True,
+        blank=True,
+        help_text="Link to access the publication, e.g., inspirehep.net link.",
+    )
+    misc = models.JSONField(
+        null=True, blank=True, help_text="Additional optional information."
+    )
 
 
 class OperatorRelation(Base):
+    """Table storing information between different oprator representation bridging scales.
+
+    For example, this quark operator maps to the following nucleon operators.
+    """
 
     source = models.ForeignKey(
-        Operator, on_delete=models.CASCADE, related_name="source_for"
+        Operator,
+        on_delete=models.CASCADE,
+        related_name="source_for",
+        help_text="More fundamental operator as a source for the propagation of scales.",
     )
     target = models.ForeignKey(
-        Operator, on_delete=models.CASCADE, related_name="target_of"
+        Operator,
+        on_delete=models.CASCADE,
+        related_name="target_of",
+        help_text="Operator as a source for the propagation of scales.",
     )
-    factor = models.TextField()
-    references = models.ManyToManyField(Reference)
-    scheme = models.CharField(max_length=256)
-    # order = models.JSONField()
+    factor = models.TextField(
+        help_text="Factor associated with the propagation of scales."
+        " E.g., 'source -> factor * target' at 'order'."
+    )
+    order = models.JSONField(
+        null=True,
+        blank=True,
+        help_text="Additional information allowing to order different operators"
+        " by their relevance."
+        " E.g., chiral power counting scheme.",
+    )
+    references = models.ManyToManyField(
+        Publication, help_text="Publications specifying the operator relationship."
+    )
+    tag = models.CharField(
+        max_length=256,
+        help_text="Key for grouping different schemes to form a complete representation"
+        " (e.g., if an expansion scheme is workout over several publications.)",
+    )
