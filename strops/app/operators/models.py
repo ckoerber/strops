b52874877ebd209aa879dff25247c4daa12eb536
ckoerber <software@ckoerber.com>
2020-06-29 15:20:25 +0200

    Further update models

--- strops/app/operators/models.py
+++ strops/app/operators/models.py
@@ -2,14 +2,20 @@
 from django.db import models
 from espressodb.base.models import Base
 
-from sympy import S, Symbol
+from sympy import S
+from sympy.physics.quantum import Operator
+
+
+SCALES = [("q", "quark"), ("n", "nucleon"), ("nrn", "Non-relativistic nuclear scale")]
 
 
 class Field(Base):
     """Field representing a degree of freedom present in operators."""
 
     kind = models.CharField(
-        max_length=256, help_text="Kind of operator like 'quark'  or 'nucleon'."
+        max_length=256,
+        choices=SCALES,
+        help_text="Kind of operator like 'quark'  or 'nucleon'.",
     )
     name = models.CharField(
         max_length=256, help_text="Descriptive name like 'up quark' or 'proton'."
@@ -25,7 +31,7 @@ class Field(Base):
 
     def check_consistency(self):
         """Checking if d.o.f. can be converted to a symbol."""
-        Symbol(self.symbol)
+        Operator(self.symbol)
 
     class Meta:
         """Constraints on class."""
@@ -34,6 +40,11 @@ class Field(Base):
 
     def __str__(self):
         return self.name
+
+    def as_op(self):
+        """
+        """
+        return Operator(self.symbol)
 
 
 class Operator(Base):
@@ -94,6 +105,13 @@ class Operator(Base):
         """
         S(self.expression)
 
+        if self.fields.values("kind").distinct() > 1:
+            raise ValueError
+
+    @property
+    def scale(self):
+        return self.fields.values("kind").first()
+
 
 class Basis(Base):
     """Basis table associating field entries with operator entries."""
@@ -145,6 +163,49 @@ class Publication(Base):
     )
 
 
+class ExpansionScheme(Base):
+    """An expansion scheme which relates operators at different scales.
+
+    It must provides keys which allow ordering different opator relations.
+    """
+
+    name = models.CharField(max_length=256, help_text="Name of the expansion scheme.")
+    source_scale = models.CharField(
+        max_length=256, choices=SCALES, help_text="The source scale of the expansion."
+    )
+    target_scale = models.CharField(
+        max_length=256, choices=SCALES, help_text="The target scale of the expansion."
+    )
+    parameters = models.JSONField(
+        help_text="List of parameters which must be present for"
+        " each term (OperatorRelation) in the expansion."
+    )
+    references = models.ManyToManyField(
+        Publication, help_text="Publications specifying the operator relationship."
+    )
+
+
+class Parameter(Base):
+    """Parameters used in computation.
+
+    This class specifies numerical values to extract from parameters.
+
+    Todo:
+        Update docstrings and consistency checks.
+    """
+
+    name = models.CharField()
+    symbol = models.CharField()
+    value = models.JSONField()
+    reference = models.JSONField()
+
+    class Meta:
+        unique_together = ["name", "reference"]
+
+    def __str__(self):
+        return f"{self.name} ({self.reference})"
+
+
 class OperatorRelation(Base):
     """Table storing information between different oprator representation bridging scales.
 
@@ -177,10 +238,25 @@ class OperatorRelation(Base):
     references = models.ManyToManyField(
         Publication, help_text="Publications specifying the operator relationship."
     )
-    tag = models.CharField(
-        max_length=256,
+    scheme = models.ForeignKey(
+        ExpansionScheme,
+        on_delete=models.CASCADE,
         help_text="Key for grouping different schemes to form a complete representation"
         " (e.g., if an expansion scheme is workout over several publications)."
         " Relationships with the same tag should share the same 'order' keys"
         " to allow sorting them by relevance. ",
     )
+    parameters = models.ManyToManyField(
+        Parameter, help_text="Parameter present in the expression."
+    )
+
+    def check_consistency(self):
+        """
+        """
+        if self.target.scale != self.scheme.target_scale:
+            raise ValueError
+        if self.source.scale != self.scheme.source_scale:
+            raise ValueError
+
+        if not set(self.order.keys()) != set(self.scheme.parameters):
+            raise ValueError
