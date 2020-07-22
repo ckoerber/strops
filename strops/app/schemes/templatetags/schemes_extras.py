715d5335d6e20c41696d36e3439d7f5a2b554005
Christopher Koerber <software@ckoerber.com>
2020-07-22 19:09:44 +0200

    Factorized template

--- strops/app/schemes/templatetags/schemes_extras.py
+++ strops/app/schemes/templatetags/schemes_extras.py
@@ -19,3 +19,13 @@ def debug(el):
 @register.filter(name="zip")
 def zip_lists(a, b):
     return zip(a, b)
+
+
+@register.inclusion_tag("schemes/scale_branch_formset.html")
+def render_scale_branch_formset(branch, formset):
+    return {"branch": branch, "formset": formset}
+
+
+@register.inclusion_tag("schemes/scheme_choice_field.html")
+def render_scheme_choice_field(field):
+    return {"field": field}
