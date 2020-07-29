e56e75fd4efb73252d4e463ae50f0368303acf64
Christopher Körber <software@ckoerber.com>
2020-07-29 17:27:57 +0200

    Added further tex subs

--- strops/schemes/plotting/tex_subs.py
+++ strops/schemes/plotting/tex_subs.py
@@ -10,10 +10,22 @@ PATTERNS = [
     (compile(r"sigma"), r"𝞂"),
     (compile(r"mu"), r"𝜇"),
     (compile(r"nu"), r"𝜈"),
+    (compile(r"delta"), r"𝛅"),
+    (
+        compile(r"epsilon_ijk[\s\*]+([a-zA-Z𝞂]+)_j[\s\*]+([a-zA-Z𝞂]+)_k"),
+        r"(\g<1>⨉\g<2>)ᵢ",
+    ),
+    (compile(r"epsilon"), r"𝛆"),
+    (compile(r"v\_i2"), r"vᵢ²"),
+    (compile(r"\_i"), r"ᵢ"),
     (compile(r"\_"), r""),
     (compile(r"\*"), r" "),
     (compile(r"I"), r"i"),
     (compile(r"\s+"), r""),
+    (compile(r"p"), r"𝗽"),
+    (compile(r"n"), r"𝐧"),
+    (compile(r"u"), r"𝐮"),
+    (compile(r"d"), r"𝗱"),
 ]
 
 
--- strops/schemes/scripts/create/__init__.py
+++ strops/schemes/scripts/create/__init__.py
@@ -1,10 +1,12 @@
 """Scripts for populating the db for the first time."""
-from strops.schemes.scripts.create import arxiv_150304811  # noqa
+from strops.schemes.scripts.create import arxiv_150304811_q2n  # noqa
+from strops.schemes.scripts.create import arxiv_150304811_q2nr  # noqa
 
 
 def create_all():
     """Creates all fields, DM-2-quark operators and DM-2-nucleon operators."""
-    arxiv_150304811.create()
+    arxiv_150304811_q2n.create()
+    arxiv_150304811_q2nr.create()
 
 
 if __name__ == "__main__":
