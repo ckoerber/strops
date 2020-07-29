b46807cae75f543bb79865899e53dc59bad7b727
Christopher Körber <software@ckoerber.com>
2020-07-29 16:45:31 +0200

    Inline admin for parameter values

--- strops/parameters/admin.py
+++ strops/parameters/admin.py
@@ -1,7 +1,23 @@
-"""Admin pages for parameters models
+"""Admin pages for parameters models.
 
 On default generates list view admins for all models
 """
-from espressodb.base.admin import register_admins
+from django.contrib.admin import StackedInline, register
 
-register_admins("strops.parameters")
+from espressodb.base.admin import register_admins
+from espressodb.base.admin import ListViewAdmin as LVA
+
+from strops.parameters.models import Parameter, ParameterValue
+
+
+class ParameterValueInline(StackedInline):  # noqa
+    model = ParameterValue
+    extra = 1
+
+
+@register(Parameter)
+class ParameterAdmin(LVA):  # noqa
+    inlines = (ParameterValueInline,)
+
+
+register_admins("strops.parameters", exclude_models=["Parameter"])
