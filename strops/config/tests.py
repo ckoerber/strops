ac505933e692787ba83ed62ae161d98ddb2b949e
Christopher Körber <software@ckoerber.com>
2020-07-29 18:32:59 +0200

    Added tests for clicking on views

--- strops/config/tests.py
+++ strops/config/tests.py
@@ -1,10 +1,17 @@
-# pylint: disable=unused-import
-"""Imports all the EspressoDB tests
-"""
-from espressodb.base.tests.apps import AppTest
-from espressodb.base.tests.views.index import IndexViewTest
-from espressodb.base.tests.views.urls import URLViewTest
+"""Imports all the EspressoDB tests."""
+from espressodb.base.tests.apps import AppTest  # noqa
+from espressodb.base.tests.views.urls import URLViewTest as URLVT  # noqa
 
-from espressodb.notifications.tests import NotificationTestCase
 
-from espressodb.management.tests.commands.info import InfoCommandTest
+class URLViewTest(URLVT):
+    exclude_urls = ["notifications", "populate"]
+
+    strops_urls = ["/operators/", "/schemes/", "/schemes/operator-mapping/from/"]
+
+    def test_strops_urls(self):
+        """Tests the HTTP status of urls provided by strops.
+        """
+        for url in self.strops_urls:
+            with self.subTest(url=url):
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, 200)
--- strops/operators/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-
-
-class OperatorsTestCase(TestCase):
-    def test_scales(self):
-        for scale, verbose in SCALES:
-            self.assertNotIn("_", scale)
