2d16027e647a2cc96457ce1f0f51b4d1e3050d04
Christopher Körber <software@ckoerber.com>
2020-07-30 10:00:00 +0200

    Added strops version tag

--- strops/utils/templatetags/strops_extras.py
+++ strops/utils/templatetags/strops_extras.py
@@ -1,7 +1,10 @@
 """Utility template tags for schemes."""
-from typing import List
+from typing import List, Optional
 from django import template
+from subprocess import check_output
+from os import path
 
+from strops.config.settings import ROOT_DIR
 from strops.operators.models import SCALES as _SCALES
 from strops.utils.tex import latex
 
@@ -31,3 +34,23 @@ def zip_lists(a, b):
 @register.filter
 def map_title(strings: List[str]) -> List[str]:
     return [str(el).capitalize() for el in strings]
+
+
+@register.simple_tag
+def get_version() -> Optional[str]:
+    """Returns the current version of strops.
+
+    Requires a symlink install or to run strops directly from the repository.
+    """
+    commit = None
+    try:
+        git_dir = path.join(ROOT_DIR, ".git")
+        if path.exists(git_dir):
+            commit = (
+                check_output(["git", f"--git-dir={git_dir}", "describe", "--always"])
+                .strip()
+                .decode()
+            )
+    except Exception:
+        pass
+    return commit
